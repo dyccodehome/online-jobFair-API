f896d55e3f89c819fdfd690682e484e33fb1df11
B17041910邓益聪 <764525691@qq.com>
2020-06-15 19:40:25 +0800

    init

---
+++ src/test/java/com/experiment/jobfair/JobfairApplicationTests.java
@@ -0,0 +1,13 @@
+package com.experiment.jobfair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobfairApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
